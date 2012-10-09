ccb4c4ef3d9bcfeae472b000a0cd441a298cdb24
Brian May <brian@microcomaustralia.com.au>
2012-10-09 16:26:56 +1100

    Add LDAP classes and attributes.

--- tldap/models.py
+++ tldap/models.py
@@ -39,27 +39,58 @@ class person(tldap.base.LDAPobject):
 
     # organizationalPerson
     title = tldap.fields.CharField()
+    x121Address = tldap.fields.CharField()
+    registeredAddresss = tldap.fields.CharField()
+    destinationIndicator = tldap.fields.CharField()
+    preferredDeliveryMethod = tldap.fields.CharField()
+    telexNumber = tldap.fields.CharField()
+    teletexTerminalIdentifier = tldap.fields.CharField()
     #telephoneNumber = tldap.fields.CharField()
+    internationaliSDNNumber = tldap.fields.CharField()
     facsimileTelephoneNumber = tldap.fields.CharField()
-    # +others
+    street = tldap.fields.CharField()
+    postOfficeBox = tldap.fields.CharField()
+    postalCode = tldap.fields.CharField()
+    postalAddress = tldap.fields.CharField()
+    physicalDeliveryOfficeName = tldap.fields.CharField()
+    ou = tldap.fields.CharField()
+    st = tldap.fields.CharField()
+    l = tldap.fields.CharField()
 
     # inetOrgPerson
+    audio = tldap.fields.CharField()
+    businessCategory = tldap.fields.CharField()
+    carLicense = tldap.fields.CharField()
+    departmentNumber = tldap.fields.CharField()
+    displayName = tldap.fields.CharField()
+    employeeNumber = tldap.fields.CharField()
+    employeeType = tldap.fields.CharField()
     givenName = tldap.fields.CharField()
+    homePhone = tldap.fields.CharField()
+    homePostalAddress = tldap.fields.CharField()
+    initials = tldap.fields.CharField()
+    jpegPhoto = tldap.fields.CharField()
+    labeledURI = tldap.fields.CharField()
     mail = tldap.fields.CharField()
     manager = tldap.fields.CharField()
     mobile = tldap.fields.CharField()
     o = tldap.fields.CharField()
+    pager = tldap.fields.CharField()
     photo = tldap.fields.CharField()
+    roomNumber = tldap.fields.CharField()
+    secretary = tldap.fields.CharField()
     uid = tldap.fields.CharField()
-    # +others
-
-    # optional
+    userCertificate = tldap.fields.CharField()
+    x500uniqueIdentifier = tldap.fields.CharField()
+    preferredLanguage = tldap.fields.CharField()
+    userSMIMECertificate = tldap.fields.CharField()
+    userPKCS12 = tldap.fields.CharField()
 
     class Meta:
         base_dn = django.conf.settings.LDAP_USER_BASE
         object_classes = { 'top', 'person', 'organizationalPerson', 'inetOrgPerson', }
 
-class ldap_account(person):
+class posix_account(person):
     loginShell = tldap.fields.CharField()
     homeDirectory = tldap.fields.CharField()
     gecos = tldap.fields.CharField()
@@ -68,7 +99,40 @@ class ldap_account(person):
         base_dn = django.conf.settings.LDAP_USER_BASE
         object_classes = { 'posixAccount', 'shadowAccount', }
 
-class ldap_pwd_account(ldap_account):
+class samba_account(posix_account):
+    # sambaSamAccount
+    # uid = tldap.fields.CharField(required=True)
+    sambaSID = tldap.fields.CharField(required=True)
+    # cn = tldap.fields.CharField()
+    sambaLMPassword = tldap.fields.CharField()
+    sambaNTPassword = tldap.fields.CharField()
+    sambaPwdLastSet = tldap.fields.CharField()
+    sambaLogonTime = tldap.fields.CharField()
+    sambaLogoffTime = tldap.fields.CharField()
+    sambaKickoffTime = tldap.fields.CharField()
+    sambaPwdCanChange = tldap.fields.CharField()
+    sambaPwdMustChange = tldap.fields.CharField()
+    sambaAcctFlags = tldap.fields.CharField()
+    # displayName = tldap.fields.CharField()
+    sambaHomePath = tldap.fields.CharField()
+    sambaHomeDrive = tldap.fields.CharField()
+    sambaLogonScript = tldap.fields.CharField()
+    sambaProfilePath = tldap.fields.CharField()
+    # description = tldap.fields.CharField()
+    sambaUserWorkstations = tldap.fields.CharField()
+    sambaPrimaryGroupSID = tldap.fields.CharField()
+    sambaDomainName = tldap.fields.CharField()
+    sambaMungedDial = tldap.fields.CharField()
+    sambaBadPasswordCount = tldap.fields.CharField()
+    sambaBadPasswordTime = tldap.fields.CharField()
+    sambaPasswordHistory = tldap.fields.CharField()
+    sambaLogonHours = tldap.fields.CharField()
+
+    class Meta:
+        base_dn = django.conf.settings.LDAP_USER_BASE
+        object_classes = { 'sambaSamAccount', }
+
+class posix_pwd_account(posix_account):
     pwdAccountLockedTime = tldap.fields.CharField(required=True)
 
     class Meta:
@@ -85,6 +149,31 @@ class ad_account(person):
         base_dn = django.conf.settings.LDAP_USER_BASE
         object_classes = { 'user', }
 
+class posix_group(tldap.base.LDAPobject):
+    objectClass = tldap.fields.CharField(required=True, max_instances=None)
+
+    # posixGroup
+    cn = tldap.fields.CharField(required=True)
+    gidNumber = tldap.fields.CharField(required=True)
+    userPassword = tldap.fields.CharField()
+    memberUid = tldap.fields.CharField()
+    description = tldap.fields.CharField()
+
+    class Meta:
+        base_dn = django.conf.settings.LDAP_GROUP_BASE
+        object_classes = { 'posixGroup', }
+
+class samba_group(posix_group):
+    # posixGroup
+    # gidNumber = tldap.fields.CharField(required=True)
+    sambaSID = tldap.fields.CharField(required=True)
+    sambaGroupType = tldap.fields.CharField(required=True)
+    displayName = tldap.fields.CharField()
+    sambaSIDList = tldap.fields.CharField()
+
+    class Meta:
+        base_dn = django.conf.settings.LDAP_GROUP_BASE
+        object_classes = { 'sambaGroupMapping', }
 
 ###########
 # TESTING #
