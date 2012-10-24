79b37aaf52a1ba6f7a9cfadd3db5da2ba145b7f8
Brian May <brian@microcomaustralia.com.au>
2012-10-24 15:45:16 +1100

    Rename python-tldap to django-tldap.

--- tldap/__init__.py
+++ tldap/__init__.py
@@ -1,19 +1,19 @@
 # Copyright 2012 VPAC
 #
-# This file is part of python-tldap.
+# This file is part of django-tldap.
 #
-# python-tldap is free software: you can redistribute it and/or modify
+# django-tldap is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# python-tldap is distributed in the hope that it will be useful,
+# django-tldap is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with python-tldap  If not, see <http://www.gnu.org/licenses/>.
+# along with django-tldap  If not, see <http://www.gnu.org/licenses/>.
 
 import tldap.utils
 from tldap.query_utils import Q
@@ -22,44 +22,36 @@ from tldap.utils import DEFAULT_LDAP_ALIAS
 connections = None
 connection = None
 
-def configure(settings):
+def _configure_django():
+    # Try to use django settings
+    from django.conf import settings
+
+    # For backwards compatibility - Port any old database settings over to
+    # the new values.
+    if not hasattr(settings, 'LDAP'):
+        settings.LDAP = {}
+
+    # ok to use django settings
+    if not settings.LDAP:
+        settings.LDAP[DEFAULT_LDAP_ALIAS] = {
+            'ENGINE': 'tldap.backend.transaction',
+            'URI': settings.LDAP_URL,
+            'USER': settings.LDAP_ADMIN_USER,
+            'PASSWORD': settings.LDAP_ADMIN_PASSWORD,
+            'USE_TLS' : False,
+            'TLS_CA' : None,
+        }
+        if hasattr(settings, 'LDAP_USE_TLS'):
+            settings.LDAP[DEFAULT_LDAP_ALIAS]["USE_TLS"] = settings.LDAP_USE_TLS
+        if settings.LDAP[DEFAULT_LDAP_ALIAS]["USE_TLS"]:
+            settings.LDAP[DEFAULT_LDAP_ALIAS]["TLS_CA"] = settings.LDAP_TLS_CA
+
     global connections
     global connection
 
-    if DEFAULT_LDAP_ALIAS not in settings:
+    if DEFAULT_LDAP_ALIAS not in settings.LDAP:
         raise RuntimeError("You must define a '%s' ldap database" % DEFAULT_LDAP_ALIAS)
-    connections = tldap.utils.ConnectionHandler(settings)
+    connections = tldap.utils.ConnectionHandler(settings.LDAP)
     connection = connections[DEFAULT_LDAP_ALIAS]
-
-def _configure_django():
-    try:
-        # Try to use django settings
-        from django.conf import settings
-    except RuntimeError:
-        # django can't be loaded, don't use django settings
-        pass
-
-    else:
-        # For backwards compatibility - Port any old database settings over to
-        # the new values.
-        if not hasattr(settings, 'LDAP'):
-            settings.LDAP = {}
-
-        # ok to use django settings
-        if not settings.LDAP:
-            settings.LDAP[DEFAULT_LDAP_ALIAS] = {
-                'ENGINE': 'tldap.backend.transaction',
-                'URI': settings.LDAP_URL,
-                'USER': settings.LDAP_ADMIN_USER,
-                'PASSWORD': settings.LDAP_ADMIN_PASSWORD,
-                'USE_TLS' : False,
-                'TLS_CA' : None,
-            }
-            if hasattr(settings, 'LDAP_USE_TLS'):
-                settings.LDAP[DEFAULT_LDAP_ALIAS]["USE_TLS"] = settings.LDAP_USE_TLS
-            if settings.LDAP[DEFAULT_LDAP_ALIAS]["USE_TLS"]:
-                settings.LDAP[DEFAULT_LDAP_ALIAS]["TLS_CA"] = settings.LDAP_TLS_CA
-
-        configure(settings.LDAP)
 
 _configure_django()
--- tldap/exceptions.py
+++ tldap/exceptions.py
@@ -1,19 +1,19 @@
 # Copyright 2012 VPAC
 #
-# This file is part of python-tldap.
+# This file is part of django-tldap.
 #
-# python-tldap is free software: you can redistribute it and/or modify
+# django-tldap is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# python-tldap is distributed in the hope that it will be useful,
+# django-tldap is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with python-tldap  If not, see <http://www.gnu.org/licenses/>.
+# along with django-tldap  If not, see <http://www.gnu.org/licenses/>.
 
 class TestFailure(Exception):
     """Simulated failure for testing."""
