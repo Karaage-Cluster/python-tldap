Karaage-Cluster/python-tldap

commit a6180486dc788c6ba81dcbff1e6c9a0bbbc481f3

Process actions immediately.
Previously, would delay processing actions until commit() called.

This requires caching the attributes, and simulating the actions in
cache. Unfortunately this is very difficult to get right, particular
as certain actions can have different side effects (e.g. alter other
attributes) depending on the server.

Also we run into the problem that any errors during the commit()
phase may happen too late to abort changes to other databases. e.g.
if the Django middleware is used, any errors generated in the middleware
will not affect the other middleware transaction layers for other
databases, and they will continue to commit all results (regardless
of order of invocation).

So we process the results immediately. This means rollback is
more likely to be required when something doesn't work, which could
introduce problems if this failes. However these changes, I believe
will result in an overall simpler and more robust design.

tldap/backend/transaction.py
@@ -20,10 +20,6 @@ with a subset of the functions from the real ldap module.  Note that the async
 and sync functions are identical. When transactions are not enabled they will
 behave like the sync functions and return the same information.
 
-When transactions are enabled, the action will be delayed until commit() is
-called. This means that rollback() is normally a nop. This could also be easily
-changed to make the changes immediately and rollback if required.
-
 The current state is simulated in cache, so that functions to retrieve
 the current data should work as expected.
 
@@ -303,16 +299,20 @@ class LDAPwrapper(object):
             self.reset(forceflushcache=True)
 
     def _process(self, oncommit, onrollback, onfailure):
-        """ Add action to list. oncommit is a callback to execute on commit(),
+        """ Process action. oncommit is a callback to execute action,
         onrollback is a callback to execute if the oncommit() has been called and
         a rollback is required """
+
+        debug("---> commiting", oncommit)
+        result = self._do_with_retry(oncommit)
+
         if not self._transact:
-            result = self._do_with_retry(oncommit)
             self.reset()
-            return result
         else:
-            self._oncommit.append( (oncommit, onrollback, onfailure) )
-            return None
+            # add statement to rollback log in case something goes wrong
+            self._onrollback.insert(0, onrollback)
+
+        return result
 
     ##################################
     # Functions needing Transactions #

commit 15e14560c1fc6c992f39f4e041f4a4cc47c0d575

Reimplement onfailure().
Was broken in a6180486dc788c6ba81dcbff1e6c9a0bbbc481f3.

tldap/backend/transaction.py
@@ -253,12 +253,12 @@ class LDAPwrapper(object):
         debug("\ncommit", self._oncommit)
         try:
             # for every action ...
-            for oncommit, onrollback, _ in self._oncommit:
+            for oncommit, onrollback, onfailure in self._oncommit:
                 # execute it
                 debug("---> commiting", oncommit)
                 self._do_with_retry(oncommit)
                 # add statement to rollback log in case something goes wrong
-                self._onrollback.insert(0, onrollback)
+                self._onrollback.insert(0, (onrollback, onfailure))
         except:
             # oops. Something went wrong. Attempt to rollback.
             debug("commit failed")
@@ -279,14 +279,11 @@ class LDAPwrapper(object):
         # database as is.
         try:
             # for every rollback action ...
-            for onrollback in self._onrollback:
+            for onrollback, onfailure in self._onrollback:
                 # execute it
                 debug("rolling back", onrollback)
                 self._do_with_retry(onrollback)
-
-            for _, _, onfailure in reversed(self._oncommit):
                 if onfailure is not None:
-                    debug("failure", onfailure)
                     onfailure()
 
         except:
@@ -310,7 +307,7 @@ class LDAPwrapper(object):
             self.reset()
         else:
             # add statement to rollback log in case something goes wrong
-            self._onrollback.insert(0, onrollback)
+            self._onrollback.insert(0, (onrollback, onfailure))
 
         return result
 