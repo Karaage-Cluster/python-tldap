d0a621ceb11f4ededdb309f91a7bac4543031bd0
Brian May <brian@microcomaustralia.com.au>
2013-02-15 09:45:54 +1100

    Add full test database settings.

--- test/settings.py
+++ test/settings.py
@@ -10,6 +10,16 @@ INSTALLED_APPS = (
     'andsome',
     'andsome.layout',
 )
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'placard.db',            # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
 
 LDAP = {
     'default': {
@@ -25,7 +35,6 @@ LDAP = {
 }
 
 TEST_RUNNER='andsome.test_utils.xmlrunner.run_tests'
-DATABASE_ENGINE = 'sqlite3'
 
 ROOT_URLCONF = 'demo.urls'
 
