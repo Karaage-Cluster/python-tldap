1663456b567b37c19f19994e8aad5b8ae2b17987
Brian May <brian@microcomaustralia.com.au>
2013-08-15 14:24:44 +1000

    Add missing file.
    Was suppose to be in:
    344e4c28e50582f6990ae050163629fe70d14c95

---
+++ tldap/methods/migrations/0002_auto__add_field_counters_scheme.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Counters.scheme'
+        db.add_column('tldap_counters', 'scheme',
+                      self.gf('django.db.models.fields.CharField')(default='default', max_length=20, db_index=True),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'Counters.scheme'
+        db.delete_column('tldap_counters', 'scheme')
+
+
+    models = {
+        u'methods.counters': {
+            'Meta': {'object_name': 'Counters', 'db_table': "'tldap_counters'"},
+            'count': ('django.db.models.fields.IntegerField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '20', 'db_index': 'True'}),
+            'scheme': ('django.db.models.fields.CharField', [], {'max_length': '20', 'db_index': 'True'})
+        }
+    }
+
+    complete_apps = ['methods']
