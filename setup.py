f913950a3f5a3fd72d48e97a51b0106cc0e3211f
Brian May <brian@microcomaustralia.com.au>
2014-03-07 09:05:39 +1100

    Replace distutils with setuptools.

--- setup.py
+++ setup.py
@@ -17,46 +17,7 @@
 # You should have received a copy of the GNU General Public License
 # along with django-tldap  If not, see <http://www.gnu.org/licenses/>.
 
-from distutils.core import setup
-from distutils.command.install import INSTALL_SCHEMES
-import os
-
-def fullsplit(path, result=None):
-    """
-    Split a pathname into components (the opposite of os.path.join) in a
-    platform-neutral way.
-    """
-    if result is None:
-        result = []
-    head, tail = os.path.split(path)
-    if head == '':
-        return [tail] + result
-    if head == path:
-        return result
-    return fullsplit(head, [tail] + result)
-
-# Tell distutils to put the data_files in platform-specific installation
-# locations. See here for an explanation:
-# http://groups.google.com/group/comp.lang.python/browse_thread/thread/35ec7b2fed36eaec/2105ee4d9e8042cb
-for scheme in INSTALL_SCHEMES.values():
-    scheme['data'] = scheme['purelib']
-
-# Compile the list of packages available, because distutils doesn't have
-# an easy way to do this.
-packages, data_files = [], []
-root_dir = os.path.dirname(__file__)
-if root_dir != '':
-    os.chdir(root_dir)
-
-for code_dir in [ 'tldap' ]:
-    for dirpath, dirnames, filenames in os.walk(code_dir):
-        # Ignore dirnames that start with '.'
-        for i, dirname in enumerate(dirnames):
-            if dirname.startswith('.'): del dirnames[i]
-        if '__init__.py' in filenames:
-            packages.append('.'.join(fullsplit(dirpath)))
-        elif filenames:
-            data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
+from setuptools import setup, find_packages
 
 with open('VERSION.txt', 'r') as f:
     version = f.readline().strip()
@@ -67,7 +28,14 @@ setup(
     author = 'Brian May',
     author_email = 'brian@microcomaustralia.com.au',
     description = 'High level python LDAP Library',
+    licence = "GPL3+",
+    packages = find_packages() + [ 'tldap.test.ldap_schemas' ],
+    package_data = {
+        'tldap.test.ldap_schemas': [ '*.schema', ],
+    },
     url = "https://github.com/Karaage-Cluster/django-tldap",
-    packages = packages,
-    data_files = data_files,
+    install_requires = [
+        "django",
+        "python-ldap",
+    ]
 )
