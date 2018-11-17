c119a33bd7c5d52998a1b3b99d5f5ab35d8f75c6
Brian May <brian@linuxpenguins.xyz>
2018-11-17 15:39:33 +1100

    Update meta information

--- setup.py
+++ setup.py
@@ -38,15 +38,12 @@ setup(
     license="GPL3+",
     long_description=open('README.rst').read(),
     classifiers=[
-        "Development Status :: 5 - Production/Stable",
+        "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: "
             "GNU General Public License v3 or later (GPLv3+)",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     keywords="ldap",
