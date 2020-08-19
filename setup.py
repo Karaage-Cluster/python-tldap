3ce6cf41b63589ae120987be03eb2494c771164a
Brian May <brian@linuxpenguins.xyz>
2020-08-20 08:25:50 +1000

    Upgrade to Python 3.8
    Change-Id: I1ee667f54c87623a30e10de4b2cbaffa2240c47a

--- setup.py
+++ setup.py
@@ -69,6 +69,7 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     keywords="ldap",
