5d370a23f79c0a845cf37a91dbe339f1ac1353e4
Brian May <brian@linuxpenguins.xyz>
2023-10-28 09:31:39 +1100

    Retry poetry2nix

--- flake.nix
+++ flake.nix
@@ -4,15 +4,16 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
   inputs.poetry2nix = {
-    url = "github:nix-community/poetry2nix";
+    # url = "github:nix-community/poetry2nix";
+    url = "github:sciyoshi/poetry2nix/new-bootstrap-fixes";
     inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, flake-utils, poetry2nix }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        # see https://github.com/nix-community/poetry2nix/tree/master#api for more functions and examples.
-        inherit (poetry2nix.legacyPackages.${system}) mkPoetryApplication;
+        p2n = import poetry2nix { inherit pkgs; };
+        mkPoetryApplication = p2n.mkPoetryApplication;
         pkgs = nixpkgs.legacyPackages.${system};
         slapd = pkgs.writeShellScriptBin "slapd" ''
           exec ${pkgs.openldap}/libexec/slapd "$@"
@@ -30,12 +31,7 @@
         };
 
         devShells.default = pkgs.mkShell {
-          packages = [
-            poetry2nix.packages.${system}.poetry
-            pkgs.libffi
-            slapd
-            pkgs.openldap
-          ];
+          packages = [ pkgs.poetry pkgs.libffi slapd pkgs.openldap ];
         };
       });
 }
